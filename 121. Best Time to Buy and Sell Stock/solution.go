3d869d9594dc9c2e54d27afeff9c5a46e3585f39
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-09-10 21:12:12 -0300

    Adiciona solução para o problema "Best Time to Buy and Sell Stock" em Go e inclui o README com a descrição do problema.

---
+++ 121. Best Time to Buy and Sell Stock/solution.go
@@ -0,0 +1,15 @@
+package main
+
+func maxProfit(prices []int) int {
+	profit := 0
+	buy := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < buy {
+			buy = prices[i]
+		}
+		if prices[i]-buy > profit {
+			profit = prices[i] - buy
+		}
+	}
+	return profit
+}
